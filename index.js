4d774d03a63e2842ce710c245800564800386bfe
Yago Santos <yagossc@gmail.com>
2020-03-25 04:35:47 -0300

    Add useful json and formadata middlewares

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 // Setup doc
 const swaggerapi = require('./swagger');
