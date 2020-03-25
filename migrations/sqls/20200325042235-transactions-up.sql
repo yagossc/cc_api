d7bce839413e9bc5a727da5904383e308f3fa4d1
Yago Santos <yagossc@gmail.com>
2020-03-25 01:30:52 -0300

    Add transactions migration

---
+++ migrations/sqls/20200325042235-transactions-up.sql
@@ -0,0 +1,11 @@
+/* Replace with your SQL commands */
+CREATE TABLE transactions (
+  transaction_id UUID NOT NULL,
+  transaction_nsu TEXT NOT NULL,
+  transaction_value FLOAT(2),
+  transaction_cflag VARCHAR(1),
+  transaction_type VARCHAR(1),
+  transaction_date TIMESTAMP,
+
+  CONSTRAINT pk_transaction PRIMARY KEY (transaction_id)
+);
