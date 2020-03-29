cbbd49ad8a1727868d055dbb600db4a583fcd174
Yago Santos <yagossc@gmail.com>
2020-03-29 08:03:47 -0300

    Add migration to fix table's column names

---
+++ migrations/sqls/20200329105852-alter-transactions-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+ALTER TABLE transactions
+RENAME transaction_value TO transaction_valor;
+ALTER TABLE transactions
+RENAME transaction_cflag TO transaction_bandeira;
+ALTER TABLE transactions
+RENAME transaction_type TO transaction_modalidade;
+ALTER TABLE transactions
+RENAME transaction_date TO transaction_horario;
