2a7c8fa21e9842bc2e3ea607361b741a809309da
Yago Santos <yagossc@gmail.com>
2020-03-29 05:30:25 -0300

    Improve error handling in application's entrypoint

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Load the environment config
 const dotenv = require('dotenv');
 config = dotenv.config();
-if (config.error) process.exit(1);
+if (config.error) {
+    console.error(config.error);
+    process.exit(1);
+}
 
 // Initialize database;
 const db = require('./store/db');
@@ -13,20 +16,19 @@ db.init(function(err){
 // Execute migrations
 const migrations = require('./internal/migrations');
 err = migrations.exec_migrations();
-if (err != null) process.exit(1);
+if (err != null){
+    console.error(err);
+    process.exit(1);
+}
 
-// Server module
+// Setup and Start server
 const server = require('./api/server');
-
-// Setup server
 const s = server.setup();
-
-// Start listening
-const connection = server.start(s, process.env.TAPI_PORT);
+const listenner = server.start(s, process.env.TAPI_PORT);
 
 // Lock graceful shutdown routine
 const system = require('./internal/system');
-system.graceful_shutdown(connection, db);
+system.graceful_shutdown(listenner, db);
 
 // Setup doc
 const swaggerapi = require('./swagger');
