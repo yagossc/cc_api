1f7365607ed7a0cad34b1ccdd94791a1b7236e58
Yago Santos <yagossc@gmail.com>
2020-04-05 05:18:11 -0300

    Add 'next business day' calculation

--- internal/withdraw_date.js
+++ internal/withdraw_date.js
@@ -6,5 +6,22 @@ module.exports.get = function(date, modalidade) {
         return withdraw_date;
     }
 
-    return modalidade == 'debito' ? withdraw_date.add_days(3) : withdraw_date.add_days(30);
+    return modalidade == 'debito' ? next_business_day(withdraw_date) : next_business_day(withdraw_date.add_days(30));
+}
+
+// next_business_day calculates the
+// next business day for a given date
+var next_business_day = function(date) {
+    var day = date.getDay();
+
+    switch(day){
+    case 0:
+        return date.add_days(1);
+        break;
+    case 6:
+        return date.add_days(2);
+        break;
+    default:
+        return date;
+    }
 }
