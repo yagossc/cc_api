dc566fd9915f48ad3ed8244a00b9d384ea8fa6b3
Yago Santos <yagossc@gmail.com>
2020-04-07 01:36:22 -0300

    Fix withdraw date for modalidade 'debito'

--- internal/withdraw_date.js
+++ internal/withdraw_date.js
@@ -7,15 +7,17 @@ module.exports.get = function(date, modalidade) {
         return withdraw_date;
     }
 
-    withdraw_date = modalidade == 'debito' ? next_business_day(withdraw_date) : next_business_day(withdraw_date.add_days(30));
+    withdraw_date = modalidade == 'debito' ? next_business_day(withdraw_date.add_days(1)) : next_business_day(withdraw_date.add_days(30));
     return format_YYYY_MM_DD(withdraw_date);
 }
 
 // format the output date as YYYY-MM-DD
 var format_YYYY_MM_DD = function(date) {
+    // Ensure day 2-digit
     let fmtday = date.getDate().toString();
     fmtday.length == 1 ? fmtday = '0'+fmtday : fmtday;
 
+    // Ensure month 2-digit
     let fmtmonth = (date.getMonth() + 1).toString();
     fmtmonth.length == 1 ? fmtmonth = '0'+fmtmonth : fmtmonth;
 
