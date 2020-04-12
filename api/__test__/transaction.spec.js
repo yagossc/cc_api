e01d126f2ffa5171dab57f5476c26ad633583f0f
Yago Santos <yagossc@gmail.com>
2020-04-12 05:09:55 -0300

    Alter api tests to follow promise chain modifications

--- api/__test__/transaction.spec.js
+++ api/__test__/transaction.spec.js
@@ -3,49 +3,39 @@ const server = require('../server');
 const db = require('../../store/db');
 
 describe("POST /transaction", function(){
-    db.mock(function(err){
-        console.error(err);
-        process.exit(1);
-    });
 
-    const migrations = require('../../internal/migrations');
-    err = migrations.exec_migrations('sqlite', 'sqlite');
-    if (err != null){
-        console.error(err);
-        process.exit(1);
-    }
+    let valid_transaction = {
+        nsu:        '0451456',
+        valor:      79.99,
+        liquido:    77.59,
+        bandeira:   'VISA',
+        modalidade: 'credito',
+        horario:    '2019-01-04T12:43:20-03:00'
+    };
+
+    let result_transaction = {
+        nsu: '0451456',
+        valor: 79.99,
+        liquido: 77.59,
+        bandeira: 'VISA',
+        modalidade: 'credito',
+        horario: '2019-01-04T12:43:20-03:00',
+        disponivel: '2019-02-04'
+    };
+
     it("returns sent transaction or an error", async function(done){
-        let valid_transaction = {
-            nsu:        '0451456',
-            valor:      79.99,
-            liquido:    77.59,
-            bandeira:   'VISA',
-            modalidade: 'credito',
-            horario:    '2019-01-04T12:43:20-03:00'
-        };
+        let initialized = await db.mock();
 
-        let result_transaction = {
-            nsu: '0451456',
-            valor: 79.99,
-            liquido: 77.59,
-            bandeira: 'VISA',
-            modalidade: 'credito',
-            horario: '2019-01-04T12:43:20-03:00',
-            disponivel: '2019-02-04'
-        };
+        const migrations = require('../../internal/migrations');
+        let migrated = await migrations.exec_migrations('sqlite', 'sqlite');
 
         let test_server = await server.init();
+
         request(test_server.app).
             post('/transaction').
             set('Accept', 'application/json').
             expect('Content-Type', /json/).
-            send({
-                nsu:        '0451456',
-                valor:      79.99,
-                bandeira:   'VISA',
-                modalidade: 'credito',
-                horario:    '2019-01-04T12:43:20-03:00'
-            }).
+            send(valid_transaction).
             expect(200, result_transaction).
             end(function(err, res){
                 console.log(res.body);
