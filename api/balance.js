880a26765774161aecb4a9c83d9fce90d5843e9e
Yago Santos <yagossc@gmail.com>
2020-04-19 21:55:00 -0300

    Promisify balance DTO

--- api/balance.js
+++ api/balance.js
@@ -5,7 +5,6 @@ const balance_dto = require('./balance_dto');
 module.exports.get_balance = async function(req, res) {
     let transactions = await transactions_store.find_all();
     let balance = await balance_dto.calculate(transactions.rows);
-    console.log("Balance: "+balance);
 
     res.json(balance);
 }
--- api/balance_dto.js
+++ api/balance_dto.js
@@ -2,19 +2,23 @@ const precision = require('../internal/fees');
 
 // calculates the balance according to current date
 module.exports.calculate = function(transactions) {
-    let balance = {};
-    balance.disponivel = 0;
-    balance.receber = 0;
+    return new Promise(resolve => {
+        let balance = {};
+        balance.disponivel = 0;
+        balance.receber = 0;
 
-    let today = new Date();
+        let today = new Date();
 
-    transactions.forEach(function(item){
-        let disponivel = new Date(item.transaction_disponivel);
-        if (disponivel < today) balance.disponivel+=item.transaction_liquido;
-        else balance.receber+=item.transaction_liquido;
-    });
-    balance.disponivel = precision.round(balance.disponivel);
-    balance.receber = precision.round(balance.receber);
+        transactions.forEach(function(item){
+            let disponivel = new Date(item.transaction_disponivel);
+            if (disponivel < today) balance.disponivel+=item.transaction_liquido;
+            else balance.receber+=item.transaction_liquido;
+        });
 
-    return balance;
+        // Round the results because javascript sucks with numbers
+        balance.disponivel = precision.round(balance.disponivel);
+        balance.receber = precision.round(balance.receber);
+
+        resolve(balance);
+    })
 }
